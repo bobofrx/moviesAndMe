ad1b6557a6abd9f78149cff21ae48b6e190b0293
Boris Faroux <boris.faroux@groupeassu2000.fr>
2020-02-13 17:13:21 +0100

    Init style list film

--- components/FilmItem.js
+++ components/FilmItem.js
@@ -1,15 +1,19 @@
 // components/FilmItem.js
 
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
 
 class FilmItem extends React.Component{
   render() {
     return (
       <View style={styles.main_container}>
-          <Text style={styles.title_text}>
-              Titre du film
-          </Text>
+        <Image style={styles.image_style} />
+        <Text style={styles.title_text}>
+            Titre du film
+        </Text>
+        <Text >Vote</Text>
+        <Text >Description</Text>
+        <Text >Date de sortie</Text>
       </View>
     )
   }
@@ -17,11 +21,19 @@ class FilmItem extends React.Component{
 
 const styles = StyleSheet.create({
     main_container: {
-        height: 190
+        height: 190,
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'red'
     } ,
     title_text: {
-
+        flex: 1
+    } ,
+    image_style: {
+        flex: 3,
+        height: 30
     }
+
 })
 
 export default FilmItem
