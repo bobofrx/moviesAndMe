03bb4156289d04cd3dd6c1b97ef7c57b64f04895
Boris Faroux <boris.faroux@groupeassu2000.fr>
2020-02-13 16:40:21 +0100

    Init squelette

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Search from './components/Search';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HelloWorld!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Search />
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
