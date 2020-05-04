c9ec6b90bd31b99cbdd97ae2bbd6b68d334e4d2f
bobofrx <boris.faroux@gmail.com>
2020-05-04 19:06:46 +0200

    Ajout tabnavigator

---
+++ components/Favorites.js
@@ -0,0 +1,24 @@
+// components/Favorites.js
+
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+class Favorites extends React.Component {
+
+    render() {
+        return (
+            <View style={styles.main_container}>
+                
+            </View>
+            
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    main_container: {
+        flex: 1
+    }
+})
+
+export default Favorites
--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,9 +1,13 @@
 // navigation/Navigation.js
 
+import React from 'react'
+import { StyleSheet, Image } from 'react-native'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import Search from '../components/Search'
 import FilmDetail from '../components/FilmDetail'
+import Favorites from '../components/Favorites'
 
 const SearchStackNavigator = createStackNavigator({
     Search: { 
@@ -20,4 +24,48 @@ const SearchStackNavigator = createStackNavigator({
     }
 })
 
-export default createAppContainer(SearchStackNavigator)
+const FavoriteStackNavigator = createStackNavigator({
+    Favorites : {
+        screen: Favorites,
+        navigationOptions: {
+            title: 'Favoris'
+        } 
+    }
+})
+
+const MoviesTabNavigator = createBottomTabNavigator({
+    Search : {
+        screen: SearchStackNavigator,
+        navigationOptions: {
+            tabBarIcon: () => {
+                return <Image source={require('../images/ic_search.png')} style={styles.icon} />
+            }
+        }
+    },
+    Favorites : {
+        screen: FavoriteStackNavigator,
+        navigationOptions: {
+            tabBarIcon: () => {
+                return <Image source={require('../images/ic_favorite.png')} style={styles.icon} />
+            }
+        }
+    }
+},
+{
+    tabBarOptions: {
+        activeBackgroundColor: '#DDDDDD',
+        inactiveBackgroundColor: '#FFFFFF',
+        showLabel: false,
+        showIcon: true
+    }
+}
+)
+
+const styles = StyleSheet.create({
+  icon: {
+      width: 20,
+      height: 20
+  }  
+})
+
+export default createAppContainer(MoviesTabNavigator)
